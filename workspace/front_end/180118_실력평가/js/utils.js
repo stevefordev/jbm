0a1bf2f2a8788504c07a2bccd2c39ab95900482c
steve park <32486591+stevefordev@users.noreply.github.com>
2018-01-19 18:35:18 +0900

    Add files via upload

---
+++ workspace/front_end/180118_실력평가/js/utils.js
@@ -0,0 +1,32 @@
+/*
+* 자주 사용하는 함수 묶음
+* 
+* */
+var utils = {
+    /**
+     }
+     * 입력받은 시작 값과 총 숫자 갯수 중에서 랜덤하게
+     * 선택하려 회신 준다
+     *
+     * @param start 시작 숫자
+     * @param size 숫자 갯수
+     * @returns {number}
+     */
+    getRandomNum: function (start, size) {
+        var num = Math.floor(Math.random() * size) + start;
+        console.log('num:' + num);
+        return num;
+    },
+    /**
+     * 랜덥하게 컬러 한가지 색상을 리턴 준다
+     * @returns {string} ex ) rgb( 0,0,0)"
+     */
+    getRandomColor: function () {
+        var r = utils.getRandomNum(0, 256);
+        var g = utils.getRandomNum(0, 256);
+        var b = utils.getRandomNum(0, 256);
+        var color = "rgb(" + r + "," + g + "," + b + ")";
+        console.log("color:" + color);
+        return color;
+    }
+}
